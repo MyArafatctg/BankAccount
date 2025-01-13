6832631da6e9082aedc087e0f187368996837c19
Arafat <myarafat.ctg@gmail.com>
2025-01-13 19:40:32 +0600

    2nd commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+    private String accountNumber;
+    private String accountName;
+    private double balance;
+
+    //Constrator
+    public BankAccount(String accountNumber, String accountName, double balance){
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    };
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposite(double amount) {
+        if (amount > 0){
+            balance += amount;
+        }
+    }
+
+    public boolean withdraw(double amount){
+        if (amount > 0 && amount <= balance){
+            balance -=amount;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean transfer(BankAccount recipient, double amount){
+        if (this.withdraw(amount)){
+            recipient.deposite(amount);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    static double totalBalance;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<BankAccount> accounts = new ArrayList<>();
+        BankAccount account1 = new BankAccount("acc123","Arafat",500.00);
+        BankAccount account2 = new BankAccount("acc456","Yasin",700.00);
+
+        account1.deposite(200);
+        account1.withdraw(100);
+        account1.transfer(account1,150);
+
+        accounts.add(account1);
+        accounts.add(account2);
+
+        for (BankAccount account : accounts) {
+            totalBalance += account.getBalance();
+        }
+        System.out.println(totalBalance);
     }
 }
